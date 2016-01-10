073cdf8c9d62567552a73f7541f65454e2ecc020
joshuagruenstein <joshuagruenstein@gmail.com>
2016-01-10 10:52:54 -0500

    reorg

--- index.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-	$("submitButton").click(function() {
-		for (var i = 1; i < 10; i++) {
-			var total = ($("#"+i).text()) * ($("#"+i+"val").text())
-			$("#"+i+"tot").text(total);
-		}
-	})
-})
