8d57043bbedf40b610fd68887110178bd2be96ad
FlyingGraysons <hmwildermuth@gmail.com>
2016-01-09 22:49:05 -0500

    Added the first three
    Started work

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
 	$("submitButton").click(function() {
-		
+		for (var i = 1; i < 10; i++) {
+			var total = ($("#"+i).text()) * ($("#"+i+"val").text())
+			$("#"+i+"tot").text(total);
+		}
 	})
 })
