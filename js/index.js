aaa3c15dbf51562206fd89be60bdbd6f792f4c47
FlyingGraysons <hmwildermuth@gmail.com>
2016-03-01 13:01:57 -0500

    Fixed indenting
    closes #4

--- js/index.js
+++ js/index.js
@@ -1,33 +1,33 @@
 $('.btn-number').click(function(e){
-		e.preventDefault();
+	e.preventDefault();
 
-		fieldName = $(this).attr('data-field');
-		type      = $(this).attr('data-type');
-		var input = $("input[name='"+fieldName+"']");
-		var currentVal = parseInt(input.val());
-		if (!isNaN(currentVal)) {
-				if(type == 'minus') {
+	fieldName = $(this).attr('data-field');
+	type      = $(this).attr('data-type');
+	var input = $("input[name='"+fieldName+"']");
+	var currentVal = parseInt(input.val());
+	if (!isNaN(currentVal)) {
+		if(type == 'minus') {
 
-						if(currentVal > input.attr('min')) {
-								input.val(currentVal - 1).change();
-						}
-						if(parseInt(input.val()) == input.attr('min')) {
-								$(this).attr('disabled', true);
-						}
+			if(currentVal > input.attr('min')) {
+				input.val(currentVal - 1).change();
+			}
+			if(parseInt(input.val()) == input.attr('min')) {
+				$(this).attr('disabled', true);
+			}
 
-				} else if(type == 'plus') {
+		} else if(type == 'plus') {
 
-						if(currentVal < input.attr('max')) {
-								input.val(currentVal + 1).change();
-						}
-						if(parseInt(input.val()) == input.attr('max')) {
-								$(this).attr('disabled', true);
-						}
+			if(currentVal < input.attr('max')) {
+				input.val(currentVal + 1).change();
+			}
+			if(parseInt(input.val()) == input.attr('max')) {
+				$(this).attr('disabled', true);
+			}
 
-				}
-		} else {
-				input.val(0);
 		}
+	} else {
+		input.val(0);
+	}
 });
 
 $('.input-number').focusin(function(){
@@ -36,57 +36,57 @@ $('.input-number').focusin(function(){
 
 $('.input-number').change(function() {
 
-		minValue =  parseInt($(this).attr('min'));
-		maxValue =  parseInt($(this).attr('max'));
-		valueCurrent = parseInt($(this).val());
+	minValue =  parseInt($(this).attr('min'));
+	maxValue =  parseInt($(this).attr('max'));
+	valueCurrent = parseInt($(this).val());
 
-		name = $(this).attr('name');
+	name = $(this).attr('name');
 
-		if(valueCurrent >= minValue) {
-				$(".btn-number[data-type='minus'][data-field='"+name+"']").removeAttr('disabled')
+	if(valueCurrent >= minValue) {
+		$(".btn-number[data-type='minus'][data-field='"+name+"']").removeAttr('disabled')
+	} else {
+		alert('Sorry, the minimum value was reached');
+		$(this).val($(this).data('oldValue'));
+	}
+	if(valueCurrent <= maxValue) {
+		$(".btn-number[data-type='plus'][data-field='"+name+"']").removeAttr('disabled')
+	} else {
+		alert('Sorry, the maximum value was reached');
+		$(this).val($(this).data('oldValue'));
+	}
+
+	if ($(this).attr('name') == 'scaledTeleop' || $(this).attr('name') == 'challengedTeleop') {
+		towerPoints = parseInt($("input[name='scaledTeleop']").val()) + parseInt($("input[name='challengedTeleop']").val());
+
+		if (towerPoints > 3) {
+			$(".btn-number[data-type='plus'][data-field='scaledTeleop']").attr('disabled', true)
+			$(".btn-number[data-type='plus'][data-field='challengedTeleop']").attr('disabled', true)
+			alert('Sorry, the maximum value was reached');
+			$(this).val($(this).data('oldValue'));
+		} else if (towerPoints == 3) {
+			$(".btn-number[data-type='plus'][data-field='scaledTeleop']").attr('disabled', true)
+			$(".btn-number[data-type='plus'][data-field='challengedTeleop']").attr('disabled', true)
 		} else {
-				alert('Sorry, the minimum value was reached');
-				$(this).val($(this).data('oldValue'));
+			$(".btn-number[data-type='plus'][data-field='scaledTeleop']").removeAttr('disabled', false)
+			$(".btn-number[data-type='plus'][data-field='challengedTeleop']").removeAttr('disabled', false)
 		}
-		if(valueCurrent <= maxValue) {
-				$(".btn-number[data-type='plus'][data-field='"+name+"']").removeAttr('disabled')
-		} else {
-				alert('Sorry, the maximum value was reached');
-				$(this).val($(this).data('oldValue'));
-		}
-
-		if ($(this).attr('name') == 'scaledTeleop' || $(this).attr('name') == 'challengedTeleop') {
-				towerPoints = parseInt($("input[name='scaledTeleop']").val()) + parseInt($("input[name='challengedTeleop']").val());
-
-				if (towerPoints > 3) {
-						$(".btn-number[data-type='plus'][data-field='scaledTeleop']").attr('disabled', true)
-						$(".btn-number[data-type='plus'][data-field='challengedTeleop']").attr('disabled', true)
-						alert('Sorry, the maximum value was reached');
-						$(this).val($(this).data('oldValue'));
-				} else if (towerPoints == 3) {
-						$(".btn-number[data-type='plus'][data-field='scaledTeleop']").attr('disabled', true)
-						$(".btn-number[data-type='plus'][data-field='challengedTeleop']").attr('disabled', true)
-				} else {
-						$(".btn-number[data-type='plus'][data-field='scaledTeleop']").removeAttr('disabled', false)
-						$(".btn-number[data-type='plus'][data-field='challengedTeleop']").removeAttr('disabled', false)
-				}
-		}
+	}
 });
 
 $(".input-number").keydown(function (e) {
-		// Allow: backspace, delete, tab, escape, enter and .
-		if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 190]) !== -1 ||
-				 // Allow: Ctrl+A
-				(e.keyCode == 65 && e.ctrlKey === true) ||
-				 // Allow: home, end, left, right
-				(e.keyCode >= 35 && e.keyCode <= 39)) {
-						 // let it happen, don't do anything
-						 return;
-		}
-		// Ensure that it is a number and stop the keypress
-		if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
-				e.preventDefault();
-		}
+	// Allow: backspace, delete, tab, escape, enter and .
+	if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 190]) !== -1 ||
+		 // Allow: Ctrl+A
+		(e.keyCode == 65 && e.ctrlKey === true) ||
+		 // Allow: home, end, left, right
+		(e.keyCode >= 35 && e.keyCode <= 39)) {
+			 // let it happen, don't do anything
+			 return;
+	}
+	// Ensure that it is a number and stop the keypress
+	if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+		e.preventDefault();
+	}
 });
 
 function recalculateSums() {
@@ -140,6 +140,6 @@ function recalculateSums() {
 $(document).ready(function() {
 	recalculateSums();
 	$('.container :input').change(function(e) {
-		recalculateSums();
+	recalculateSums();
 	});
 })
